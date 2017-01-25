63b6f53cbf4adb4cef9553d0b5ef32786a02daee
adam-wsknski <a.wysokinski@yahoo.com>
2017-01-25 11:19:23 +0100

    Fixed styling of microposts, added controller and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124160916) do
+ActiveRecord::Schema.define(version: 20170125085315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at", using: :btree
+    t.index ["user_id"], name: "index_microposts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -28,33 +37,8 @@ ActiveRecord::Schema.define(version: 20170124160916) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
-    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
-  create_table "weather", id: false, force: :cascade do |t|
-    t.date    "cet"
-    t.integer "temperatura_maksymalnac"
-    t.integer "temperatura_sredniac"
-    t.integer "temperatura_minimalnac"
-    t.integer "punkt_rosyc"
-    t.integer "meandew_pointc"
-    t.integer "min_dewpointc"
-    t.integer "max_wilgotnosc"
-    t.integer "mean_wilgotnosc"
-    t.integer "min_wilgotnosc"
-    t.float   "max_cisnienie_na_poziomi_morzahpa"
-    t.float   "mean_cisnienie_na_poziomie_morzahpa"
-    t.float   "min_cisnienie_na_poziomie_morzahpa_"
-    t.integer "max_widzialnosckm"
-    t.integer "mean_widzialnosckm"
-    t.integer "min_widzialnosckm"
-    t.integer "max_predkosc_wiatru_km_h"
-    t.integer "mean_predkosc_wiatru_km_h"
-    t.integer "max_predkosc_w_porywie_km_h"
-    t.float   "opadymm"
-    t.integer "cloudcover"
-    t.string  "wydarzenia",                          limit: 50
-    t.integer "winddirdegrees"
-  end
-
+  add_foreign_key "microposts", "users"
 end
